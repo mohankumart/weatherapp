866e31af54f743cfd66a290fe261b6d0cf1aa89e
Mohan Kumar <tmk_mohankumar@yahoo.com>
2018-04-02 16:02:36 +0530

    app file

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-var getUser = (id, callback) => {
-    var user = {
-        id: id,
-        name: 'Mohan'
+const request = require('request')
+const yargs = require('yargs')
+
+const argv = yargs.options({
+    a: {
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch waether info',
+        string: true
+    }    
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+let address = encodeURIComponent(argv.a)
+console.log(address)
+request({
+    'url': `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
+    json: true
+
+}, (error, response, body)=>{
+    if(error){
+        console.log(error)
+    }else if(body.status == 'ZERO_RESULTS'){
+       console.log('error in request')
+    }else if(body.status == 'OK'){
+        console.log('response is ok')
     }
-
-    setTimeout(()=>{
-        callback(user)
-    },3000)
     
-};
-
-
-getUser(31, (user)=>{
-console.log(user);
-});
+})
