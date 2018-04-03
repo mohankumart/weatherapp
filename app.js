d42fd09adf786bbe2fb2d95659c8e156c7f96717
Mohan Kumar <tmk_mohankumar@yahoo.com>
2018-04-03 11:26:43 +0530

    changed

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-const request = require('request')
 const yargs = require('yargs')
+const geocode = require('./geocode/geocode')
+
+// const argv = yargs.options({
+//     a: {
+//         demand: true,
+//         alias: 'address',
+//         describe: 'Address to fetch waether info',
+//         string: true
+//     }    
+// })
+// .help()
+// .alias('help', 'h')
+// .argv;
 
 const argv = yargs.options({
-    a: {
+    latitude: {
         demand: true,
-        alias: 'address',
-        describe: 'Address to fetch waether info',
-        string: true
-    }    
-})
-.help()
-.alias('help', 'h')
-.argv;
-
-let address = encodeURIComponent(argv.a)
-console.log(address)
-request({
-    'url': `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
-    json: true
-
-}, (error, response, body)=>{
-    if(error){
-        console.log(error)
-    }else if(body.status == 'ZERO_RESULTS'){
-       console.log('error in request')
-    }else if(body.status == 'OK'){
-        console.log('response is ok')
+        describe: 'laitude'
+    },
+    longitude: {
+        demand: true,
+        describe: 'longitude'
     }
-    
+}).argv
+
+// geocode.geocodeAddress(argv.address,(errorMessage, results)=>{
+//     console.log(results)
+// })
+
+geocode.getCurrentTemperature(argv.latitude, argv.longitude, (errorMessage, results) => {
+    console.log(results)
 })
---
+++ geocode/geocode.js
@@ -0,0 +1,41 @@
+const request = require('request')
+
+module.exports.geocodeAddress = (addres, callback) => {
+    let address = encodeURIComponent(addres)
+    console.log(address)
+    request({
+        'url': `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
+        json: true
+    
+    }, (error, response, body)=>{
+        console.log(response)
+        if(error){
+            console.log(error)
+        }else if(body.status == 'ZERO_RESULTS'){
+           console.log('zero results')
+        }else if(body.status == 'OK'){
+            console.log('response is ok')
+            console.log(body)
+            callback(undefined, body)
+        }
+    })
+}
+
+module.exports.getCurrentTemperature = (latitude, longitude, callback) => {
+    request({
+        'url': `https://api.darksky.net/forecast/4a445c93da234cba04b36ec20ad82045/${latitude},${longitude}`,
+        json: true
+    
+    }, (error, response, body)=>{
+        console.log(response)
+        if(error){
+            console.log(error)
+        }else if(body.status == 'ZERO_RESULTS'){
+           console.log('zero results')
+        }else if(body.status == 'OK'){
+            console.log('response is ok')
+            console.log(body)
+            callback(undefined, body)
+        }
+    })
+}
